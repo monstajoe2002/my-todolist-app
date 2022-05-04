2f31909bceabdc520466a83f5c9c6d50217e4e1d
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-05 01:22:18 +0200

    Todo list application using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState,useRef,useEffect } from 'react';
+import TodoList from './TodoList';
+import {v4} from 'uuid';
 function App() {
+  const LOCAL_STORAGE_KEY='todoApp.todos'
+  const [todos,setTodos]=useState([])
+  const todoNameRef=useRef()
+  useEffect(()=>{
+    const storedTodos=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+  },[todos])
+  function toggleTodo(id)
+  {
+    const newTodos=[...todos]
+    const todo=newTodos.find(todo=>todo.id===id)
+    todo.completed=!todo.completed
+    setTodos(newTodos)
+  }
+  function handleAddTodo(e) {
+    const name=todoNameRef.current.value
+    if(name==='')return
+    setTodos(prevTodos=>{
+      return [...prevTodos, { id: v4(),name:name,completed:false}]
+    })
+    todoNameRef.current.value=null
+  }
+  function handleClearTodos(e)
+  {
+    const newTodos=todos.filter(todo=>!todo.completed)
+    setTodos(newTodos)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TodoList todos={todos} toggleTodo={toggleTodo}/>
+    <input ref={todoNameRef} type="text"/>
+    <button onClick={handleAddTodo}>Add Task</button>
+    <button onClick={handleClearTodos}>Clear All completed Tasks</button>
+    <button>Select All</button>
+    <div>{todos.filter(todo=>!todo.completed).length} task(s) left</div>
+    </>
+    
   );
 }
 
